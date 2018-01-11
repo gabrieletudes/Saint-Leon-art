a158004686a475f4b710740930a6c7c49b880b3f
Gabriel M <gabrieletudes@gmail.com>
2018-01-11 18:38:28 +0100

    Added index page

--- st-leonart/wp-content/themes/stleonarttheme/index.php
+++ st-leonart/wp-content/themes/stleonarttheme/index.php
@@ -82,139 +82,165 @@
           <img src="http://fillmurray.com/462/300" alt="The image next">
         </div><!--END-image next-->
       </div><!--END Content wrapper-->
-      <div class="o-content-wrapper content-wrapper--light-purple o-flex o-flex--wrap">
-      <div class="o-content-wrapper__item o-flex u-padding-vertical-large o-flex--wrap">
-        <h3 class="c-h--yellow o-flex u-1/1@mobile">Nos artistes vedette</h3>
-        <div class="c-artistcard u-padding-right-small">
-          <img class="c-artistcard__image o-flex__item" src="http://fillmurray.com/224/224" alt="there is not image yet">
-          <span class="c-artistcard__name o-flex">Chaineux Jocelyne</span>
-          <span class="c-artistcard__ability o-flex">Photographe, peintre figurative</span>
-        </div>
-        <div class="c-artistcard u-padding-right-small">
-          <img class="c-artistcard__image o-flex__item" src="http://fillmurray.com/224/224" alt="there is not image yet">
-          <span class="c-artistcard__name o-flex">Chaineux Jocelyne</span>
-          <span class="c-artistcard__ability o-flex">Photographe, peintre figurative</span>
-        </div>
-      </div><!--END-Artiste vedette-->
+      <div class="o-content-wrapper c-bg-color--purple-light-20 o-flex o-flex--wrap u-padding-vertical-large c-bg-color--purple-light-40">
+        <div class="o-flex u-padding-vertical-large o-flex--wrap u-1/1 u-padding-horizontal c-bg-color--shadow-white">
+          <h3 class="c-h--yellow o-flex u-1/1@mobile">Nos artistes vedette</h3>
+          <a href="#" class="c-artistcard u-padding-right-small u-margin-bottom-small">
+            <img class="c-artistcard__image u-margin-bottom-small o-flex__item" src="http://fillmurray.com/220/220" alt="there is not image yet">
+            <span class="c-artistcard__name o-flex">Chaineux Jocelyne</span>
+            <span class="c-artistcard__ability o-flex">Photographe, peintre figurative</span>
+          </a>
+          <a href="#" class="c-artistcard u-padding-right-small u-margin-bottom-small">
+            <img class="c-artistcard__image u-margin-bottom-small o-flex__item" src="http://fillmurray.com/220/220" alt="there is not image yet">
+            <span class="c-artistcard__name o-flex">Chaineux Jocelyne</span>
+            <span class="c-artistcard__ability o-flex">Photographe, peintre figurative</span>
+          </a>
+          <a href="#" class="c-artistcard u-padding-right-small u-margin-bottom-small">
+            <img class="c-artistcard__image u-margin-bottom-small o-flex__item" src="http://fillmurray.com/220/220" alt="there is not image yet">
+            <span class="c-artistcard__name o-flex">Chaineux Jocelyne</span>
+            <span class="c-artistcard__ability o-flex">Photographe, peintre figurative</span>
+          </a>
+          <a href="#" class="c-artistcard u-padding-right-small u-margin-bottom-small">
+            <img class="c-artistcard__image u-margin-bottom-small o-flex__item" src="http://fillmurray.com/220/220" alt="there is not image yet">
+            <span class="c-artistcard__name o-flex">Chaineux Jocelyne</span>
+            <span class="c-artistcard__ability o-flex">Photographe, peintre figurative</span>
+          </a>
+        </div><!--END-Artiste vedette-->
       </div><!--END Content wrapper-->
-      <div class="section-wrapper o-wrapper o-flex o-wrapper--tiny u-padding-vertical-large">
-        <section class="events-section">
-          <h3 class="u-my-420">NOS PROCHAINS ÉVÉNEMENTS</h3>
-          <article class="c-event o-flex u-margin-bottom-small">
-            <a class="o-flex" href="#">
-              <time datetime="2017-11-03" class="c-event__date o-flex__item o-flex u-2/12@mobile o-flex--column o-flex--centered">
-                <span class="o-flex__item">oct</span>
-                <span class="o-flex__item momo">28</span>
-                <span class="o-flex__item">2017</span>
-              </time>
-              <div class="o-flex o-flex--wrap u-margin-left-small">
-                <h4 class="o-flex__item u-margin-bottom-tiny">Atelier de l’artiste Musselman avec Musselman</h4>
-                <address class="o-flex__item u-margin-bottom-tiny">
-                  36, Place Vivegnis 4000 Liège
-                </address>
-              </div>
+      <div class="o-content-wrapper--max o-flex o-flex--wrap o-flex--reversed">
+        <div class="content-wrapper--light-purple o-flex o-flex--wrap u-1/1 u-2/3@desktop o-content-wrapper--padding-right u-padding-vertical-large">
+          <section class="events-section u-1/1@mobile u-1/1 o-flex o-flex--wrap">
+            <h3 class="u-my-420 u-1/1">NOS PROCHAINS ÉVÉNEMENTS</h3>
+            <article class="c-event o-flex u-margin-bottom-small u-1/1@mobile u-1/2@desktop">
+              <a class="o-flex" href="#">
+                <time datetime="2017-11-03" class="c-event__date o-flex__item o-flex u-2/12@mobile o-flex--column o-flex--centered">
+                  <span class="o-flex__item">oct</span>
+                  <span class="o-flex__item c-event__date-day">28</span>
+                  <span class="o-flex__item">2017</span>
+                </time>
+                <div class="o-flex o-flex--wrap u-margin-left-small">
+                  <h4 class="o-flex__item u-margin-bottom-tiny">Atelier de l’artiste Musselman avec Musselman</h4>
+                  <address class="o-flex__item u-margin-bottom-tiny">
+                    36, Place Vivegnis 4000 Liège
+                  </address>
+                </div>
+              </a>
+            </article>
+            <article class="c-event o-flex u-margin-bottom-small u-1/1@mobile u-1/2@desktop">
+              <a class="o-flex" href="#">
+                <time datetime="2017-11-03" class="c-event__date o-flex__item o-flex u-2/12@mobile o-flex--column o-flex--centered">
+                  <span class="o-flex__item">oct</span>
+                  <span class="o-flex__item c-event__date-day">28</span>
+                  <span class="o-flex__item">2017</span>
+                </time>
+                <div class="o-flex o-flex--wrap u-margin-left-small">
+                  <h4 class="o-flex__item u-margin-bottom-tiny">Atelier de l’artiste Musselman avec Musselman</h4>
+                  <address class="o-flex__item u-margin-bottom-tiny">
+                    36, Place Vivegnis 4000 Liège
+                  </address>
+                </div>
+              </a>
+            </article>
+            <article class="c-event o-flex u-margin-bottom-small u-1/1@mobile u-1/2@desktop">
+              <a class="o-flex" href="#">
+                <time datetime="2017-11-03" class="c-event__date o-flex__item o-flex u-2/12@mobile o-flex--column o-flex--centered">
+                  <span class="o-flex__item">oct</span>
+                  <span class="o-flex__item c-event__date-day">28</span>
+                  <span class="o-flex__item">2017</span>
+                </time>
+                <div class="o-flex o-flex--wrap u-margin-left-small">
+                  <h4 class="o-flex__item u-margin-bottom-tiny">Atelier de l’artiste Musselman avec Musselman</h4>
+                  <address class="o-flex__item u-margin-bottom-tiny">
+                    36, Place Vivegnis 4000 Liège
+                  </address>
+                </div>
+              </a>
+            </article>
+          </section>
+          <a href="#" class="cta-button cta-button--on-inversed u-padding-">Consultez le programme</a>
+        </div><!--END Events section-->
+        <div class="section-wrapper o-wrapper o-flex o-wrapper--tiny o-flex--wrap u-1/1 u-1/3@desktop o-content-wrapper--padding-left u-padding-vertical-large c-bg-color--purple-light-05">
+          <h3 class="u-1/1 c-h--white">Nos plus recent posts sur Instagram</h3>
+          <div class="o-flex o-flex--wrap u-my-270">
+            <a class="c-insta__element  u-padding-bottom-small u-padding-right-small" href="#">
+              <img src="http://fillmurray.com/90/90" alt="Image de instagrame">
             </a>
-          </article>
-          <article class="c-event o-flex u-margin-bottom-small">
-            <a class="o-flex" href="#">
-              <time datetime="2017-11-03" class="c-event__date o-flex__item o-flex u-2/12@mobile o-flex--column o-flex--centered">
-                <span class="o-flex__item">oct</span>
-                <span class="o-flex__item momo">28</span>
-                <span class="o-flex__item">2017</span>
-              </time>
-              <div class="o-flex o-flex--wrap u-margin-left-small">
-                <h4 class="o-flex__item u-margin-bottom-tiny">Atelier de l’artiste Musselman avec Musselman</h4>
-                <address class="o-flex__item u-margin-bottom-tiny">
-                  36, Place Vivegnis 4000 Liège
-                </address>
-              </div>
+            <a class="c-insta__element  u-padding-bottom-small u-padding-right-small" href="#">
+              <img src="http://fillmurray.com/90/90" alt="Image de instagrame">
             </a>
-          </article>
-          <article class="c-event o-flex u-margin-bottom-small">
-            <a class="o-flex" href="#">
-              <time datetime="2017-11-03" class="c-event__date o-flex__item o-flex u-2/12@mobile o-flex--column o-flex--centered">
-                <span class="o-flex__item">oct</span>
-                <span class="o-flex__item momo">28</span>
-                <span class="o-flex__item">2017</span>
-              </time>
-              <div class="o-flex o-flex--wrap u-margin-left-small">
-                <h4 class="o-flex__item u-margin-bottom-tiny">Atelier de l’artiste Musselman avec Musselman</h4>
-                <address class="o-flex__item u-margin-bottom-tiny">
-                  36, Place Vivegnis 4000 Liège
-                </address>
-              </div>
+            <a class="c-insta__element  u-padding-bottom-small u-padding-right-small" href="#">
+              <img src="http://fillmurray.com/90/90" alt="Image de instagrame">
             </a>
-          </article>
-          <a href="#" class="cta-button cta-button--on-inversed">Consultez le programme</a>
-        </section>
-      </div><!--END Events section-->
-      <div class="content-wrapper o-flex o-flex--wrap">
-      <div class="section-wrapper o-wrapper o-flex o-wrapper--tiny u-padding-vertical-large o-flex--wrap">
-        <h3 class="o-flex u-1/1">Nos plus recent posts sur Instagram</h3>
-        <a class="partners-list__element o-flex__item u-1/3@mobile o-flex u-margin-bottom-small" href="#">
-          <img src="http://fillmurray.com/90/90" alt="Image de instagrame">
-        </a>
-        <a class="partners-list__element o-flex__item u-1/3@mobile o-flex u-margin-bottom-small" href="#">
-          <img src="http://fillmurray.com/90/90" alt="Image de instagrame">
-        </a>
-        <a class="partners-list__element o-flex__item u-1/3@mobile o-flex u-margin-bottom-small" href="#">
-          <img src="http://fillmurray.com/90/90" alt="Image de instagrame">
-        </a>
-        <a class="partners-list__element o-flex__item u-1/3@mobile o-flex u-margin-bottom-small" href="#">
-          <img src="http://fillmurray.com/90/90" alt="Image de instagrame">
-        </a>
-        <a class="partners-list__element o-flex__item u-1/3@mobile o-flex u-margin-bottom-small" href="#">
-          <img src="http://fillmurray.com/90/90" alt="Image de instagrame">
-        </a>
-        <a class="partners-list__element o-flex__item u-1/3@mobile o-flex u-margin-bottom-small" href="#">
-          <img src="http://fillmurray.com/90/90" alt="Image de instagrame">
-        </a>
-        <div class="">
-          <h4>Suivez-nous sur</h4>
-          <ul class="o-flex o-flex--wrap">
-            <li class="partners-list__element o-flex__item u-1/4@mobile o-flex u-margin-right-small">
-              <a href="#">
-                <img src="http://via.placeholder.com/53x53" alt="Icon">
+            <a class="c-insta__element  u-padding-bottom-small u-padding-right-small" href="#">
+              <img src="http://fillmurray.com/90/90" alt="Image de instagrame">
+            </a>
+            <a class="c-insta__element  u-padding-bottom-small u-padding-right-small" href="#">
+              <img src="http://fillmurray.com/90/90" alt="Image de instagrame">
+            </a>
+            <a class="c-insta__element o-flex__item u-margin-bottom-small u-padding-right-small" href="#">
+              <img src="http://fillmurray.com/90/90" alt="Image de instagrame">
+            </a>
+          </div>
+          <div class="u-1/1">
+            <h4 class="c-h--white">Suivez-nous sur</h4>
+            <div class="o-flex o-flex--wrap c-social-nav o-flex--centered">
+              <a class="c-social-nav__item u-1/4  o-flex__item" href="#">
+                <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--fab" aria-hidden="true">
+                </span><span class="u-hidden-visually">Facebook</span>
               </a>
-            </li>
-            <li class="partners-list__element o-flex__item u-1/4@mobile o-flex u-margin-right-small">
-              <a href="#">
-                <img src="http://via.placeholder.com/53x53" alt="Icon">
+              <a class="c-social-nav__item u-1/4 o-flex__item" href="#">
+                <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--twtr" aria-hidden="true">
+                </span><span class="u-hidden-visually">Twitter</span>
               </a>
-            </li>
-            <li class="partners-list__element o-flex__item u-1/4@mobile o-flex u-margin-right-small">
-              <a href="#">
-                <img src="http://via.placeholder.com/53x53" alt="Icon">
+              <a class="c-social-nav__item u-1/4 o-flex__item" href="#">
+                <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--inst" aria-hidden="true">
+                </span><span class="u-hidden-visually">Instagram</span>
               </a>
-            </li>
-            <li class="partners-list__element o-flex__item u-1/4@mobile o-flex u-margin-right-small">
-              <a href="#">
-                <img src="http://via.placeholder.com/53x53" alt="Icon">
+              <a class="c-social-nav__item u-1/4 o-flex__item" href="#">
+                <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--mail" aria-hidden="true">
+                </span><span class="u-hidden-visually">Mail</span>
               </a>
-            </li>
-          </ul>
-        </div>
-      </div><!--END Insta section-->
-    </div><!--END Content Wrapper-->
-      <div class="section-wrapper o-wrapper o-flex o-wrapper--tiny u-padding-vertical-large o-flex--wrap">
-        <h3 class="o-flex u-1/1">Liste de nos partenaires</h3>
-        <a class="partners-list__element o-flex__item u-1/3@mobile o-flex u-margin-bottom-small" href="#">
-          <img src="http://via.placeholder.com/76x60?text=Our+partner" alt="Logo de notre partenaire">
+            </div>
+          </div>
+        </div><!--END Insta section-->
+      </div>
+      <div class="c-partner o-content-wrapper content-wrapper--light-purple o-flex o-flex--wrap u-padding-vertical-large c-bg-color--purple-light-15 o-flex--lock-left">
+        <h3 class="o-flex u-1/1 c-h--white">Liste de nos partenaires</h3>
+        <a class="c-partner__link u-padding-bottom-small u-padding-right-small" href="#">
+          <img src="./assets/img/partners/arsenic.svg" alt="Logo de notre partenaire">
+      <!--<object type="image/svg+xml" data="./src/img/logos-partners.svg#vervinckt">
+          SVG n'est pas supporte sur votre navigateur, utilisez un navigateur plus rescent
+      </object>-->
         </a>
-        <a class="partners-list__element o-flex__item u-1/3@mobile o-flex u-margin-bottom-small" href="#">
-          <img src="http://via.placeholder.com/76x60?text=Our+partner" alt="Logo de notre partenaire">
+        <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+          <img src="./assets/img/partners/calg.svg" alt="Logo de notre partenaire">
         </a>
-        <a class="partners-list__element o-flex__item u-1/3@mobile o-flex u-margin-bottom-small" href="#">
-          <img src="http://via.placeholder.com/76x60?text=Our+partner" alt="Logo de notre partenaire">
+        <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+          <img src="./assets/img/partners/cpcr.svg" alt="Logo de notre partenaire">
         </a>
-        <a class="partners-list__element o-flex__item u-1/3@mobile o-flex u-margin-bottom-small" href="#">
-          <img src="http://via.placeholder.com/76x60?text=Our+partner" alt="Logo de notre partenaire">
+        <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+          <img src="./assets/img/partners/lehangar.svg" alt="Logo de notre partenaire">
         </a>
-        <a class="partners-list__element o-flex__item u-1/3@mobile o-flex u-margin-bottom-small" href="#">
-          <img src="http://via.placeholder.com/76x60?text=Our+partner" alt="Logo de notre partenaire">
+        <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+          <img src="./assets/img/partners/liege.svg" alt="Logo de notre partenaire">
         </a>
-        <a class="partners-list__element o-flex__item u-1/3@mobile o-flex u-margin-bottom-small" href="#">
-          <img src="http://via.placeholder.com/76x60?text=Our+partner" alt="Logo de notre partenaire">
+        <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+          <img src="./assets/img/partners/naos.svg" alt="Logo de notre partenaire">
+        </a>
+        <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+          <img src="./assets/img/partners/pac.svg" alt="Logo de notre partenaire">
+        </a>
+        <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+          <img src="./assets/img/partners/province.svg" alt="Logo de notre partenaire">
+        </a>
+        <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+          <img src="./assets/img/partners/ravi.svg" alt="Logo de notre partenaire">
+        </a>
+        <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+          <img src="./assets/img/partners/st-leonard.svg" alt="Logo de notre partenaire">
+        </a>
+        <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+          <img src="./assets/img/partners/vervinckt.svg" alt="Logo de notre partenaire">
         </a>
       </div><!--END Partners section-->
       <div class="site-cache" id="site-cache"></div>
