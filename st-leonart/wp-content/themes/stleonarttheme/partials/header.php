cd43dd42873cf89d0dbb1b19df3b9e0930da4521
Gabriel M <gabrieletudes@gmail.com>
2018-01-11 18:39:34 +0100

    Moved header to header partial

---
+++ st-leonart/wp-content/themes/stleonarttheme/partials/header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="fr" id="jsPage" class="no-js">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0 minimum-scale=1.0" />
+  <link rel="stylesheet" href="assets/css/inuitcss/main.css">
+  <link rel="shortcut icon" href="/favicon.png">
+  <title>Accueil - Saint Leon'art</title>
+</head>
+<body>
+  <div class="mr-hidder">
+    <input class="u-hidden" type="checkbox" id="sidebartoggler" name="toggler" value="">
+    <header class="c-header o-flex o-flex--lock-top o-flex--wrap">
+      <div class="c-header__wrapper haha o-flex u-1/1 o-flex--wrap o-flex--space-between u-padding-horizontal-tiny u-padding-top-tiny">
+        <label for="sidebartoggler" role="menubar" class="c-header__icon c-header__icon--menu o-flex o-flex--centered o-margin-left-tiny" id="c-header__icon"></label>
+        <a href="#" class="o-block o-flex c-header__logo c-social-nav--hidde"><img class="u-padding-tiny" src="assets/img/logo.svg" alt="Logo" width="62" height="49">
+        </a>
+        <a href="#" class="c-header__icon c-header__icon--search o-flex o-margin-left-tiny c-social-nav--hidde o-flex--centered"></a>
+        <div class="o-flex o-flex--wrap c-social-nav u-padding-left c-social-nav--hidde-m o-flex--centered">
+          <a class="c-social-nav__item u-1/4  o-flex__item" href="#">
+            <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--fab" aria-hidden="true">
+            </span><span class="u-hidden-visually">Facebook</span>
+          </a>
+          <a class="c-social-nav__item u-1/4 o-flex__item" href="#">
+            <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--twtr" aria-hidden="true">
+            </span><span class="u-hidden-visually">Twitter</span>
+          </a>
+          <a class="c-social-nav__item u-1/4 o-flex__item" href="#">
+            <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--inst" aria-hidden="true">
+            </span><span class="u-hidden-visually">Instagram</span>
+          </a>
+          <a class="c-social-nav__item u-1/4 o-flex__item" href="#">
+            <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--mail" aria-hidden="true">
+            </span><span class="u-hidden-visually">Mail</span>
+          </a>
+        </div>
+        <nav class="c-header__nav titi o-flex o-flex__item u-2/3@mobile u-4/12@tablet u-11/12@desktop o-flex--lock-top-left o-flex--wrap">
+          <a href="#" class="o-block o-flex c-header__nav-logo u-margin-right-small"><img class="u-padding-tiny" src="assets/img/logo.svg" alt="Logo" width="62" height="49"></a>
+          <a class="c-header__nav-item c-header__nav-item--home o-flex__item u-margin-right" href="index.php">Accueil</a>
+          <a class="c-header__nav-item c-header__nav-item--agenda o-flex__item u-margin-right" href="programme.php">Programme</a>
+          <a class="c-header__nav-item c-header__nav-item--about o-flex__item u-margin-right" href="apropos.php">Saint Leon'art</a>
+          <a class="c-header__nav-item c-header__nav-item--artists o-flex__item u-margin-right" href="#">Artistes</a>
+          <a class="c-header__nav-item c-header__nav-item--news o-flex__item u-margin-right" href="#">Articles</a>
+          <a class="c-header__nav-item c-header__nav-item--handy o-flex__item u-margin-right" href="#">Pratique</a>
+          <a class="c-header__nav-item c-header__nav-item--contact o-flex__item u-margin-right" href="#">Contact</a>
+          <div class="o-flex o-flex--wrap c-social-nav u-margin-top-large u-padding-left c-social-nav--hidde">
+            <a class="c-social-nav__item u-1/4  o-flex__item" href="#">
+              <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--fab" aria-hidden="true">
+              </span><span class="u-hidden-visually">Facebook</span>
+            </a>
+            <a class="c-social-nav__item u-1/4 o-flex__item" href="#">
+              <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--twtr" aria-hidden="true">
+              </span><span class="u-hidden-visually">Twitter</span>
+            </a>
+            <a class="c-social-nav__item u-1/4 o-flex__item" href="#">
+              <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--inst" aria-hidden="true">
+              </span><span class="u-hidden-visually">Instagram</span>
+            </a>
+            <a class="c-social-nav__item u-1/4 o-flex__item" href="#">
+              <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--mail" aria-hidden="true">
+              </span><span class="u-hidden-visually">Mail</span>
+            </a>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <div class="site-pusher">
+      <div class="o-flex c-header__back o-content-wrapper o-flex--wrap o-flex--centered  o-flex--lock-left">
+        <span class="u-1/1@mobile u-6/12@tablet u-5/12@desktop u-3/12@wide">
+        <h1 class="c-header__title o-flex__item o-flex o-flex--wrap u-padding-small u-11/12@mobile u-10/12 u-margin-bottom-none"><span class="u-1/1">Saint Léon’art </span><small class="u-1/1 c-header__title--small">Un parcours artistique à Liège</small></h1>
+        <a href="#" class="o-flex__item o-flex o-flex--wrap c-header__sub-title u-padding-small">Venez decouvrir nos événements <span class="o-flex__item u-1/1">Du 24 out au 30 octobre a Liege</span>
+        </a>
+        </span>
+      </div>
