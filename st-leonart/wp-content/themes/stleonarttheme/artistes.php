8765d4dd1c06ac9ead092a21c8d093d7ad771dbc
Gabriel M <gabrieletudes@gmail.com>
2018-01-13 19:37:51 +0100

    Added artistes page

---
+++ st-leonart/wp-content/themes/stleonarttheme/artistes.php
@@ -0,0 +1,96 @@
+<?php include('partials/header.php')?>
+<div class="o-content-wrapper c-bg-color--purple-light-20 o-flex o-flex--wrap u-padding-vertical-large c-bg-color--purple-light-40">
+  <div role="navigation" aria-label="menu pour les expositions" class="o-wrapper--small c-bg-color--purple u-1/1 u-margin-bottom">
+    <ul role="menubar" class="c-nav-event o-flex u-1/1 u-margin-vertical-small u-margin-horizontal-none o-flex--lock-right o-flex--wrap">
+      <li role="menuitem" tabindex="0" class="o-flex__item c-nav-event__li">organizer par<a aria-haspopup="true" class="c-nav-event__option c-nav-event__option--icon-down u-padding-small">profession</a>
+        <ul role="menu" aria-hidden="true" aria-label="submenu" class="c-nav-event__drop-menu c-nav-event__drop-menu--right u-margin-none o-flex o-flex--wrap">
+          <li tabindex="-1" class="o-flex u-1/1"><a class="u-1/1 u-padding-small" href="#">Photographe</a></li>
+          <li tabindex="-1" class="o-flex u-1/1"><a class="u-1/1 u-padding-small" href="#">Graphiste</a></li>
+          <li tabindex="-1" class="o-flex u-1/1"><a class="u-1/1 u-padding-small" href="#">Peintre</a></li>
+          <li tabindex="-1" class="o-flex u-1/1"><a class="u-1/1 u-padding-small" href="#">Musicien</a></li>
+          <li tabindex="-1" class="o-flex u-1/1"><a class="u-1/1 u-padding-small" href="#">Écrivain</a></li>
+          <li tabindex="-1" class="o-flex u-1/1"><a class="u-1/1 u-padding-small" href="#">Graffeur</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+  <div class="o-flex u-padding-vertical-large o-flex--wrap u-1/1 u-padding-horizontal c-bg-color--shadow-white">
+    <h3 class="c-h--yellow o-flex u-1/1@mobile">Nos artistes</h3>
+    <a href="#" class="c-artistcard u-padding-right-small u-margin-bottom-small u-1/2@mobile u-1/4@tablet u-1/4@desktop u-2/12@wide">
+      <img class="c-artistcard__image u-margin-bottom-small o-flex__item" src="http://fillmurray.com/320/320" alt="there is not image yet">
+      <span class="c-artistcard__name o-flex">Chaineux Jocelyne</span>
+      <span class="c-artistcard__ability o-flex">Photographe, peintre figurative</span>
+    </a>
+    <a href="#" class="c-artistcard u-padding-right-small u-margin-bottom-small u-1/2@mobile u-1/4@tablet u-1/4@desktop u-2/12@wide">
+      <img class="c-artistcard__image u-margin-bottom-small o-flex__item" src="http://fillmurray.com/320/320" alt="there is not image yet">
+      <span class="c-artistcard__name o-flex">Chaineux Jocelyne</span>
+      <span class="c-artistcard__ability o-flex">Photographe, peintre figurative</span>
+    </a>
+    <a href="#" class="c-artistcard u-padding-right-small u-margin-bottom-small u-1/2@mobile u-1/4@tablet u-1/4@desktop u-2/12@wide">
+      <img class="c-artistcard__image u-margin-bottom-small o-flex__item" src="http://fillmurray.com/320/320" alt="there is not image yet">
+      <span class="c-artistcard__name o-flex">Chaineux Jocelyne</span>
+      <span class="c-artistcard__ability o-flex">Photographe, peintre figurative</span>
+    </a>
+    <a href="#" class="c-artistcard u-padding-right-small u-margin-bottom-small u-1/2@mobile u-1/4@tablet u-1/4@desktop u-2/12@wide">
+      <img class="c-artistcard__image u-margin-bottom-small o-flex__item" src="http://fillmurray.com/320/320" alt="there is not image yet">
+      <span class="c-artistcard__name o-flex">Chaineux Jocelyne</span>
+      <span class="c-artistcard__ability o-flex">Photographe, peintre figurative</span>
+    </a>
+    <a href="#" class="c-artistcard u-padding-right-small u-margin-bottom-small u-1/2@mobile u-1/4@tablet u-1/4@desktop u-2/12@wide">
+      <img class="c-artistcard__image u-margin-bottom-small o-flex__item" src="http://fillmurray.com/320/320" alt="there is not image yet">
+      <span class="c-artistcard__name o-flex">Chaineux Jocelyne</span>
+      <span class="c-artistcard__ability o-flex">Photographe, peintre figurative</span>
+    </a>
+    <a href="#" class="c-artistcard u-padding-right-small u-margin-bottom-small u-1/2@mobile u-1/4@tablet u-1/4@desktop u-2/12@wide">
+      <img class="c-artistcard__image u-margin-bottom-small o-flex__item" src="http://fillmurray.com/320/320" alt="there is not image yet">
+      <span class="c-artistcard__name o-flex">Chaineux Jocelyne</span>
+      <span class="c-artistcard__ability o-flex">Photographe, peintre figurative</span>
+    </a>
+    <a href="#" class="c-artistcard u-padding-right-small u-margin-bottom-small u-1/2@mobile u-1/4@tablet u-1/4@desktop u-2/12@wide">
+      <img class="c-artistcard__image u-margin-bottom-small o-flex__item" src="http://fillmurray.com/320/320" alt="there is not image yet">
+      <span class="c-artistcard__name o-flex">Chaineux Jocelyne</span>
+      <span class="c-artistcard__ability o-flex">Photographe, peintre figurative</span>
+    </a>
+  </div><!--END-Nos artistes-->
+</div><!--END Content wrapper-->
+<div class="c-partner o-content-wrapper content-wrapper--light-purple o-flex o-flex--wrap u-padding-vertical-large c-bg-color--purple-light-15 o-flex--lock-left">
+  <h3 class="o-flex u-1/1 c-h--white">Liste de nos partenaires</h3>
+  <a class="c-partner__link u-padding-bottom-small u-padding-right-small" href="#">
+    <img src="./assets/img/partners/arsenic.svg" alt="Logo de notre partenaire">
+    <!--<object type="image/svg+xml" data="./src/img/logos-partners.svg#vervinckt">
+    SVG n'est pas supporte sur votre navigateur, utilisez un navigateur plus rescent
+  </object>-->
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/calg.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/cpcr.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/lehangar.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/liege.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/naos.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/pac.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/province.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/ravi.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/st-leonard.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/vervinckt.svg" alt="Logo de notre partenaire">
+</a>
+</div><!--END Partners section-->
+<div class="site-cache" id="site-cache"></div>
+<?php include('partials/footer.php')?>
