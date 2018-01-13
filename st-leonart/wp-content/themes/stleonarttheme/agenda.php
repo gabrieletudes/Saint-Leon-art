88713a4db8813b52057088147602693ac11be949
Gabriel M <gabrieletudes@gmail.com>
2018-01-13 19:36:47 +0100

    Added agenda page

---
+++ st-leonart/wp-content/themes/stleonarttheme/agenda.php
@@ -0,0 +1,207 @@
+<?php include('partials/header.php')?>
+<div class="o-content-wrapper o-content-wrapper--max o-flex o-flex--wrap u-padding-top-small">
+  <div role="navigation" aria-label="menu pour les expositions" class="o-wrapper--small c-bg-color--purple u-1/1">
+    <ul role="menubar" class="c-nav-event o-flex u-1/1 u-margin-vertical-small u-margin-horizontal-none o-flex--space-between o-flex--wrap">
+      <li role="menuitem" tabindex="0" class="o-flex__item c-nav-event__li">
+        <a href="#" aria-haspopup="true" class="c-nav-event__option c-nav-event__option--icon-down u-padding-small">Ordre</a>
+        <ul role="menu" tabindex="-1" aria-hidden="true" aria-label="submenu" class="c-nav-event__drop-menu c-nav-event__drop-menu--left u-margin-none o-flex o-flex--wrap">
+          <li role="menuitem" tabindex="-1" class="o-flex u-1/1"><a class="u-1/1 u-padding-small" href="#">Thematique</a></li>
+          <li role="menuitem" tabindex="-1" class="o-flex u-1/1"><a class="u-1/1 u-padding-small" href="#">Chronologique</a></li>
+        </ul>
+      </li>
+      <li class="">organizer par<a href="#" class="c-nav-event__option u-padding-small">jour</a>
+        <span class="o-flex__item c-nav-event__li" role="menuitem" tabindex="0">
+        <a aria-haspopup="true" class="c-nav-event__option c-nav-event__option--icon-down u-padding-small">Lieu</a>
+        <ul role="menu" aria-hidden="true" aria-label="submenu" class="c-nav-event__drop-menu c-nav-event__drop-menu--right u-margin-none o-flex o-flex--wrap">
+          <li tabindex="-1" class="o-flex u-1/1">
+            <a class="u-1/1 u-padding-small" href="#">Liege</a>
+          </li>
+          <li tabindex="-1" class="o-flex u-1/1">
+            <a class="u-1/1 u-padding-small" href="#">Ans</a>
+          </li>
+          <li tabindex="-1" class="o-flex u-1/1">
+            <a class="u-1/1 u-padding-small" href="#">Mons</a>
+          </li>
+          <li tabindex="-1" class="o-flex u-1/1">
+            <a class="u-1/1 u-padding-small" href="#">Outremeuse</a>
+          </li>
+          <li tabindex="-1" class="o-flex u-1/1">
+            <a class="u-1/1 u-padding-small" href="#">Curtius Museum</a>
+          </li>
+          <li tabindex="-1" class="o-flex u-1/1">
+            <a class="u-1/1 u-padding-small" href="#">L'îlot Saint-Georges</a>
+          </li>
+        </ul>
+        </span>
+      </li>
+    </ul>
+  </div>
+  <div class="o-content--wrapper u-padding-vertical-large o-flex o-flex--wrap u-1/1">
+    <div class="u-7/12@desktop u-padding-right">
+      <h2 class="u-my-420">Programme</h2>
+      <p class="u-my-420">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est eopksio laborum. Sed ut perspiciatis unde omnis istpoe natus error sit voluptatem.</p>
+    </div>
+    <div class="u-5/12@desktop">
+      <h2 class="u-my-420">L'edition précédente</h2>
+      <dl class="o-flex u-1/1@desktop u-7/12@wide u-margin-bottom-none">
+        <div class="c-event-last o-flex o-flex--wrap-reverse u-1/3">
+          <dt class="c-event-last__data o-flex o-flex--centered o-flex__item u-1/1">Participants</dt>
+          <dd class="c-event-last__desc o-flex o-flex--centered o-flex__item u-1/1 u-margin-none">3000</dd>
+        </div>
+        <div class="c-event-last o-flex o-flex--wrap-reverse u-1/3">
+          <dt class="c-event-last__data o-flex o-flex--centered o-flex__item u-1/1">Artistes</dt>
+          <dd class="c-event-last__desc o-flex o-flex--centered o-flex__item u-1/1 u-margin-none">1000</dd>
+        </div>
+        <div class="c-event-last o-flex o-flex--wrap-reverse u-1/3">
+          <dt class="c-event-last__data o-flex o-flex--centered o-flex__item u-1/1">Événements</dt>
+          <dd class="c-event-last__desc o-flex o-flex--centered o-flex__item u-1/1 u-margin-none">300</dd>
+        </div>
+      </dl>
+    </div>
+    <p class="u-my-420"></p>
+  </div><!--END-intro text-->
+</div>
+<div class="o-content-wrapper o-flex o-flex--wrap u-padding-bottom-large">
+  <section class="o-flex o-flex--wrap">
+    <div class="c-section__title-wrapper o-flex u-1/1">
+      <h3 class="c-h--purple o-flex o-flex--centered-v">Programme du<span class="u-hidden-visually">28/10/2017</span></h3>
+      <select class="c-h c-event-type u-margin-bottom" id="date" name="date">
+        <option class="c-event-type__elmnt" value="2017-10-28"><time class="u-margin-left-tiny" datetime="2017-10-28">28/10/2017</time></option>
+        <option class="c-event-type__elmnt" value="2017-10-28"><time class="u-margin-left-tiny" datetime="2017-10-28">29/10/2017</time></option>
+        <option class="c-event-type__elmnt" value="2017-10-28"><time class="u-margin-left-tiny" datetime="2017-10-28">30/10/2017</time></option>
+        <option class="c-event-type__elmnt" value="2017-10-28"><time class="u-margin-left-tiny" datetime="2017-10-28">31/10/2017</time></option>
+      </select>
+    </div>
+    <article class="c-event o-flex u-margin-bottom-small u-1/1@mobile u-1/2@desktop">
+      <a class="o-flex" href="#">
+        <time datetime="2017-11-03" class="c-event__date o-flex__item o-flex u-2/12@mobile o-flex--column o-flex--centered">
+          <span class="o-flex__item">oct</span>
+          <span class="o-flex__item c-event__date-day">28</span>
+          <span class="o-flex__item">2017</span>
+        </time>
+        <div class="o-flex o-flex--wrap u-margin-left-small">
+          <h4 class="o-flex__item u-margin-bottom-tiny">Atelier de l’artiste Musselman avec Musselman</h4>
+          <address class="o-flex__item u-margin-bottom-tiny">
+            36, Place Vivegnis 4000 Liège
+          </address>
+        </div>
+      </a>
+    </article>
+    <article class="c-event o-flex u-margin-bottom-small u-1/1@mobile u-1/2@desktop">
+      <a class="o-flex" href="#">
+        <time datetime="2017-11-03" class="c-event__date o-flex__item o-flex u-2/12@mobile o-flex--column o-flex--centered">
+          <span class="o-flex__item">oct</span>
+          <span class="o-flex__item c-event__date-day">28</span>
+          <span class="o-flex__item">2017</span>
+        </time>
+        <div class="o-flex o-flex--wrap u-margin-left-small">
+          <h4 class="o-flex__item u-margin-bottom-tiny">Atelier de l’artiste Musselman avec Musselman</h4>
+          <address class="o-flex__item u-margin-bottom-tiny">
+            36, Place Vivegnis 4000 Liège
+          </address>
+        </div>
+      </a>
+    </article>
+    <article class="c-event o-flex u-margin-bottom-small u-1/1@mobile u-1/2@desktop">
+      <a class="o-flex" href="#">
+        <time datetime="2017-11-03" class="c-event__date o-flex__item o-flex u-2/12@mobile o-flex--column o-flex--centered">
+          <span class="o-flex__item">oct</span>
+          <span class="o-flex__item c-event__date-day">28</span>
+          <span class="o-flex__item">2017</span>
+        </time>
+        <div class="o-flex o-flex--wrap u-margin-left-small">
+          <h4 class="o-flex__item u-margin-bottom-tiny">Atelier de l’artiste Musselman avec Musselman</h4>
+          <address class="o-flex__item u-margin-bottom-tiny">
+            36, Place Vivegnis 4000 Liège
+          </address>
+        </div>
+      </a>
+    </article>
+    <article class="c-event o-flex u-margin-bottom-small u-1/1@mobile u-1/2@desktop">
+      <a class="o-flex" href="#">
+        <time datetime="2017-11-03" class="c-event__date o-flex__item o-flex u-2/12@mobile o-flex--column o-flex--centered">
+          <span class="o-flex__item">oct</span>
+          <span class="o-flex__item c-event__date-day">28</span>
+          <span class="o-flex__item">2017</span>
+        </time>
+        <div class="o-flex o-flex--wrap u-margin-left-small">
+          <h4 class="o-flex__item u-margin-bottom-tiny">Atelier de l’artiste Musselman avec Musselman</h4>
+          <address class="o-flex__item u-margin-bottom-tiny">
+            36, Place Vivegnis 4000 Liège
+          </address>
+        </div>
+      </a>
+    </article>
+    <article class="c-event o-flex u-margin-bottom-small u-1/1@mobile u-1/2@desktop">
+      <a class="o-flex" href="#">
+        <time datetime="2017-11-03" class="c-event__date o-flex__item o-flex u-2/12@mobile o-flex--column o-flex--centered">
+          <span class="o-flex__item">oct</span>
+          <span class="o-flex__item c-event__date-day">28</span>
+          <span class="o-flex__item">2017</span>
+        </time>
+        <div class="o-flex o-flex--wrap u-margin-left-small">
+          <h4 class="o-flex__item u-margin-bottom-tiny">Atelier de l’artiste Musselman avec Musselman</h4>
+          <address class="o-flex__item u-margin-bottom-tiny">
+            36, Place Vivegnis 4000 Liège
+          </address>
+        </div>
+      </a>
+    </article>
+    <article class="c-event o-flex u-margin-bottom-small u-1/1@mobile u-1/2@desktop">
+      <a class="o-flex" href="#">
+        <time datetime="2017-11-03" class="c-event__date o-flex__item o-flex u-2/12@mobile o-flex--column o-flex--centered">
+          <span class="o-flex__item">oct</span>
+          <span class="o-flex__item c-event__date-day">28</span>
+          <span class="o-flex__item">2017</span>
+        </time>
+        <div class="o-flex o-flex--wrap u-margin-left-small">
+          <h4 class="o-flex__item u-margin-bottom-tiny">Atelier de l’artiste Musselman avec Musselman</h4>
+          <address class="o-flex__item u-margin-bottom-tiny">
+            36, Place Vivegnis 4000 Liège
+          </address>
+        </div>
+      </a>
+    </article>
+  </section>
+</div><!--END Content wrapper-->
+<div class="c-partner o-content-wrapper content-wrapper--light-purple o-flex o-flex--wrap u-padding-vertical-large c-bg-color--purple-light-15 o-flex--lock-left">
+  <h3 class="o-flex u-1/1 c-h--white">Liste de nos partenaires</h3>
+  <a class="c-partner__link u-padding-bottom-small u-padding-right-small" href="#">
+    <img src="./assets/img/partners/arsenic.svg" alt="Logo de notre partenaire">
+    <!--<object type="image/svg+xml" data="./src/img/logos-partners.svg#vervinckt">
+    SVG n'est pas supporte sur votre navigateur, utilisez un navigateur plus rescent
+  </object>-->
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/calg.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/cpcr.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/lehangar.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/liege.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/naos.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/pac.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/province.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/ravi.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/st-leonard.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/vervinckt.svg" alt="Logo de notre partenaire">
+</a>
+</div><!--END Partners section-->
+<div class="site-cache" id="site-cache"></div>
+<?php include('partials/footer.php')?>
