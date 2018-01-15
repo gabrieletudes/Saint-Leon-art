a9d63d13fd868bdf38ffc79014c25e2f7486c28a
Gabriel M <gabrieletudes@gmail.com>
2018-01-15 22:33:17 +0100

    Added single-pratique page

---
+++ st-leonart/wp-content/themes/stleonarttheme/pratique-single.php
@@ -0,0 +1,110 @@
+<?php include('partials/header.php')?>
+<div class="o-content-wrapper o-content-wrapper--max o-flex o-flex--wrap u-padding-bottom u-padding-top-large">
+  <h2 class="u-10/12@tablet u-8/12@desktop u-6/12@wide u-margin-bottom-tiny">La maison du mosselman</h2>
+  <div class="u-10/12@tablet u-8/12@desktop u-6/12@wide o-flex o-flex--wrap u-margin-left-tiny u-margin-bottom-tiny">
+    <p class="u-1/1 u-margin-bottom-small">36, Place Vivegnis 4000 Liège</p>
+    <p class="o-flex o-flex--centered-v u-1/1@mobile u-1/2@tablet u-margin-bottom-tiny c-practical-icon c-practical-icon--location">parking de la bois, parking g</p>
+    <p class="o-flex o-flex--centered-v u-1/1@mobile u-1/2@tablet u-margin-bottom-tiny c-practical-icon c-practical-icon--bus">bus: 2, 23, 32, 40</p>
+  </div>
+  <div class="u-1/1@mobile u-10/12@tablet u-8/12@desktop u-7/12@wide c-article">
+    <img src="http://fillmurray.com/600/268" alt="image du">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste praesentium aliquid explicabo vero minima commodi neque voluptatem molestiae veniam qui, iure necessitatibus a possimus, tempora voluptates laboriosam quam vitae nisi!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste praesentium aliquid explicabo vero minima commodi neque voluptatem molestiae veniam qui, iure necessitatibus a possimus, tempora voluptates laboriosam quam vitae nisi!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste praesentium aliquid explicabo vero minima commodi neque voluptatem molestiae veniam qui, iure necessitatibus a possimus, tempora voluptates laboriosam quam vitae nisi!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste praesentium aliquid explicabo vero minima commodi neque voluptatem molestiae veniam qui, iure necessitatibus a possimus, tempora voluptates laboriosam quam vitae nisi!</p>
+      </div>
+    </div>
+    <div class="o-content-wrapper o-flex o-flex--wrap u-padding-bottom-large">
+      <div class="content-wrapper--light-purple o-flex o-flex--wrap u-1/1 u-margin-vertical-large">
+        <section class="events-section u-1/1@mobile u-1/1 o-flex o-flex--wrap">
+          <h3 class="u-my-420 u-1/1">Les événements qui se deroulent à cette adresse</h3>
+          <article class="c-event o-flex u-margin-bottom u-1/1@mobile u-1/2@desktop">
+            <a class="o-flex" href="#">
+              <time datetime="2017-11-03" class="c-event__date o-flex__item o-flex u-2/12@mobile o-flex--column o-flex--centered">
+                <span class="o-flex__item">oct</span>
+                <span class="o-flex__item c-event__date-day">28</span>
+                <span class="o-flex__item">2017</span>
+              </time>
+              <div class="o-flex o-flex--wrap u-margin-left-small">
+                <h4 class="o-flex__item u-margin-bottom-tiny">Atelier de l’artiste Musselman avec Musselman</h4>
+                <address class="o-flex__item u-margin-bottom-tiny">
+                  36, Place Vivegnis 4000 Liège
+                </address>
+              </div>
+            </a>
+          </article>
+          <article class="c-event o-flex u-margin-bottom u-1/1@mobile u-1/2@desktop">
+            <a class="o-flex" href="#">
+              <time datetime="2017-11-03" class="c-event__date o-flex__item o-flex u-2/12@mobile o-flex--column o-flex--centered">
+                <span class="o-flex__item">oct</span>
+                <span class="o-flex__item c-event__date-day">28</span>
+                <span class="o-flex__item">2017</span>
+              </time>
+              <div class="o-flex o-flex--wrap u-margin-left-small">
+                <h4 class="o-flex__item u-margin-bottom-tiny">Atelier de l’artiste Musselman avec Musselman</h4>
+                <address class="o-flex__item u-margin-bottom-tiny">
+                  36, Place Vivegnis 4000 Liège
+                </address>
+              </div>
+            </a>
+          </article>
+          <article class="c-event o-flex u-margin-bottom u-1/1@mobile u-1/2@desktop">
+            <a class="o-flex" href="#">
+              <time datetime="2017-11-03" class="c-event__date o-flex__item o-flex u-2/12@mobile o-flex--column o-flex--centered">
+                <span class="o-flex__item">oct</span>
+                <span class="o-flex__item c-event__date-day">28</span>
+                <span class="o-flex__item">2017</span>
+              </time>
+              <div class="o-flex o-flex--wrap u-margin-left-small">
+                <h4 class="o-flex__item u-margin-bottom-tiny">Atelier de l’artiste Musselman avec Musselman</h4>
+                <address class="o-flex__item u-margin-bottom-tiny">
+                  36, Place Vivegnis 4000 Liège
+                </address>
+              </div>
+            </a>
+          </article>
+        </section>
+        <a href="#" class="cta-button c-link c-link--forward c-link--upper cta-button--on-inversed u-margin-top-small">Consultez le programme</a>
+      </div><!--END Events section-->
+    </div>
+    <div class="c-partner o-content-wrapper content-wrapper--light-purple o-flex o-flex--wrap u-padding-vertical-large c-bg-color--purple-light-15 o-flex--lock-left">
+      <h3 class="o-flex u-1/1 c-h--white">Liste de nos partenaires</h3>
+      <a class="c-partner__link u-padding-bottom-small u-padding-right-small" href="#">
+        <img src="./assets/img/partners/arsenic.svg" alt="Logo de notre partenaire">
+        <!--<object type="image/svg+xml" data="./src/img/logos-partners.svg#vervinckt">
+        SVG n'est pas supporte sur votre navigateur, utilisez un navigateur plus rescent
+      </object>-->
+    </a>
+    <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+      <img src="./assets/img/partners/calg.svg" alt="Logo de notre partenaire">
+    </a>
+    <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+      <img src="./assets/img/partners/cpcr.svg" alt="Logo de notre partenaire">
+    </a>
+    <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+      <img src="./assets/img/partners/lehangar.svg" alt="Logo de notre partenaire">
+    </a>
+    <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+      <img src="./assets/img/partners/liege.svg" alt="Logo de notre partenaire">
+    </a>
+    <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+      <img src="./assets/img/partners/naos.svg" alt="Logo de notre partenaire">
+    </a>
+    <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+      <img src="./assets/img/partners/pac.svg" alt="Logo de notre partenaire">
+    </a>
+    <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+      <img src="./assets/img/partners/province.svg" alt="Logo de notre partenaire">
+    </a>
+    <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+      <img src="./assets/img/partners/ravi.svg" alt="Logo de notre partenaire">
+    </a>
+    <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+      <img src="./assets/img/partners/st-leonard.svg" alt="Logo de notre partenaire">
+    </a>
+    <a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+      <img src="./assets/img/partners/vervinckt.svg" alt="Logo de notre partenaire">
+    </a>
+  </div><!--END Partners section-->
+  <div class="site-cache" id="site-cache"></div>
+  <?php include('partials/footer.php')?>
