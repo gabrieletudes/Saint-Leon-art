4d2367624405c8a2b4daf2fae55cbfb9fbbfe053
Gabriel M <gabrieletudes@gmail.com>
2018-01-15 22:33:40 +0100

    Added Pratique page

---
+++ st-leonart/wp-content/themes/stleonarttheme/pratique.php
@@ -0,0 +1,112 @@
+<?php include('partials/header.php')?>
+<div class="o-content-wrapper o-content-wrapper--max o-flex o-flex--wrap u-padding-vertical-large c-bg-color--yellow">
+  <div role="navigation" aria-label="menu pour les expositions" class="o-wrapper--small c-bg-color--purple u-1/1">
+    <ul role="menubar" class="c-nav-event o-flex u-1/1 u-margin-vertical-small u-margin-horizontal-none o-flex--space-between o-flex--wrap o-flex--lock-right">
+      <li role="menuitem" tabindex="0" class="o-flex__item c-nav-event__li">organizer par<a aria-haspopup="true" class="c-nav-event__option c-nav-event__option--icon-down u-padding-small">lieu</a>
+        <ul role="menu" aria-hidden="true" aria-label="submenu" class="c-nav-event__drop-menu c-nav-event__drop-menu--right u-margin-none o-flex o-flex--wrap">
+          <li tabindex="-1" class="o-flex u-1/1"><a class="u-1/1 u-padding-small" href="#">Liege</a></li>
+          <li tabindex="-1" class="o-flex u-1/1"><a class="u-1/1 u-padding-small" href="#">Outremeuse</a></li>
+          <li tabindex="-1" class="o-flex u-1/1"><a class="u-1/1 u-padding-small" href="#">Ans</a></li>
+          <li tabindex="-1" class="o-flex u-1/1"><a class="u-1/1 u-padding-small" href="#">Mons</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+  <div class="o-content--wrapper u-padding-vertical-large o-flex o-flex--wrap u-1/1">
+    <div class="u-7/12@desktop u-padding-right">
+      <h2 class="u-my-420">Information pratiques</h2>
+      <p class="u-my-420">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est eopksio laborum. Sed ut perspiciatis unde omnis istpoe natus error sit voluptatem.</p>
+    </div>
+  </div><!--END-intro text-->
+  <section class="o-flex o-flex--wrap u-1/1 o-flex--space-between">
+    <div class="c-section__title-wrapper o-flex u-1/1">
+      <h3 class="c-h--purple o-flex o-flex--centered-v">Les differents lieu</h3>
+    </div>
+    <div class="c-article o-flex u-padding-tiny u-padding-bottom u-1/1@mobile u-1/2@tablet u-1/2@desktop u-4/12@wide">
+      <a href="#" class="c-bg-color--shadow-white c-anime--hover-up">
+        <div class="c-practical__element u-margin-none">
+          <img src="http://fillmurray.com/352/144" alt="Image de l'article">
+        <div class="o-flex o-flex--wrap u-margin-left-small u-margin-bottom-small">
+          <h4 class="u-margin-bottom-tiny u-1/1 c-practical__sub-title">La maison du mosselman</h4>
+          <p class="u-margin-bottom-none">36, Place Vivegnis 4000 Liège</p>
+        </div>
+        <div class="o-flex o-flex--wrap u-margin-left-small u-margin-right-small u-margin-bottom-small">
+          <p class="o-flex o-flex--centered-v u-margin-bottom-tiny u-margin-right-large c-practical-icon c-practical-icon--location">parking de la bois, parking g</p>
+          <p class="o-flex o-flex--centered-v u-margin-bottom-none c-practical-icon c-practical-icon--bus">bus: 2, 23, 32, 40</p>
+        </div>
+        </div>
+      </a>
+    </div>
+    <div class="c-article o-flex u-padding-tiny u-padding-bottom u-1/1@mobile u-1/2@tablet u-1/2@desktop u-4/12@wide">
+      <a href="#" class="c-bg-color--shadow-white c-anime--hover-up">
+        <div class="c-practical__element u-margin-none">
+          <img src="http://fillmurray.com/352/144" alt="Image de l'article">
+        <div class="o-flex o-flex--wrap u-margin-left-small u-margin-bottom-small">
+          <h4 class="u-margin-bottom-tiny u-1/1 c-practical__sub-title">La maison du mosselman</h4>
+          <p class="u-margin-bottom-none">36, Place Vivegnis 4000 Liège</p>
+        </div>
+        <div class="o-flex o-flex--wrap u-margin-left-small u-margin-right-small u-margin-bottom-small">
+          <p class="o-flex o-flex--centered-v u-margin-bottom-tiny u-margin-right-large c-practical-icon c-practical-icon--location">parking de la bois, parking g</p>
+          <p class="o-flex o-flex--centered-v u-margin-bottom-none c-practical-icon c-practical-icon--bus">bus: 2, 23, 32, 40</p>
+        </div>
+        </div>
+      </a>
+    </div>
+    <div class="c-article o-flex u-padding-tiny u-padding-bottom u-1/1@mobile u-1/2@tablet u-1/2@desktop u-4/12@wide">
+      <a href="#" class="c-bg-color--shadow-white c-anime--hover-up">
+        <div class="c-practical__element u-margin-none">
+          <img src="http://fillmurray.com/352/144" alt="Image de l'article">
+        <div class="o-flex o-flex--wrap u-margin-left-small u-margin-bottom-small">
+          <h4 class="u-margin-bottom-tiny u-1/1 c-practical__sub-title">La maison du mosselman</h4>
+          <p class="u-margin-bottom-none">36, Place Vivegnis 4000 Liège</p>
+        </div>
+        <div class="o-flex o-flex--wrap u-margin-left-small u-margin-right-small u-margin-bottom-small">
+          <p class="o-flex o-flex--centered-v u-margin-bottom-tiny u-margin-right-large c-practical-icon c-practical-icon--location">parking de la bois, parking g</p>
+          <p class="o-flex o-flex--centered-v u-margin-bottom-none c-practical-icon c-practical-icon--bus">bus: 2, 23, 32, 40</p>
+        </div>
+        </div>
+      </a>
+    </div>
+  </section>
+</div><!--END Content wrapper-->
+<div class="c-partner o-content-wrapper content-wrapper--light-purple o-flex o-flex--wrap u-padding-vertical-large c-bg-color--purple-light-15 o-flex--lock-left">
+  <h3 class="o-flex u-1/1 c-h--white">Liste de nos partenaires</h3>
+  <a class="c-partner__link u-padding-bottom-small u-padding-right-small" href="#">
+    <img src="./assets/img/partners/arsenic.svg" alt="Logo de notre partenaire">
+    <!--<object type="image/svg+xml" data="./src/img/logos-partners.svg#vervinckt">
+    SVG n'est pas supporte sur votre navigateur, utilisez un navigateur plus rescent
+  </object>-->
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/calg.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/cpcr.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/lehangar.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/liege.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/naos.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/pac.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/province.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/ravi.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/st-leonard.svg" alt="Logo de notre partenaire">
+</a>
+<a class="c-partner__link u-padding-bottom-small u-padding-right" href="#">
+  <img src="./assets/img/partners/vervinckt.svg" alt="Logo de notre partenaire">
+</a>
+</div><!--END Partners section-->
+<div class="site-cache" id="site-cache"></div>
+<?php include('partials/footer.php')?>
