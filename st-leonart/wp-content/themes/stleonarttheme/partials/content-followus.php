7e08038403800ae9025ff3f8c5de9d0c98ac39de
Gabriel M <gabrieletudes@gmail.com>
2018-01-19 23:49:55 +0100

    Fixed visual issue with social links

--- st-leonart/wp-content/themes/stleonarttheme/partials/content-followus.php
+++ st-leonart/wp-content/themes/stleonarttheme/partials/content-followus.php
@@ -1,15 +1,15 @@
 <div class="u-1/1">
     <h4 class="c-h--white"><?=__('Suivez-nous sur','');?></h4>
-    <div class="o-flex o-flex--wrap c-social-nav o-flex--centered o-flex--lock-left">
-        <a class="c-social-nav__item u-1/4  o-flex__item" href="#">
+    <div class="o-flex o-flex--wrap c-social-nav o-flex--lock-left">
+        <a class="c-social-nav__item o-flex__item u-padding-tiny" href="#">
                 <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--fab" aria-hidden="true">
                 </span><span class="u-hidden-visually">Facebook</span>
         </a>
-        <a class="c-social-nav__item u-1/4 o-flex__item" href="#">
+        <a class="c-social-nav__item o-flex__item u-padding-tiny u-margin-left" href="#">
                 <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--twtr" aria-hidden="true">
                 </span><span class="u-hidden-visually">Twitter</span>
         </a>
-        <a class="c-social-nav__item u-1/4 o-flex__item" href="#">
+        <a class="c-social-nav__item o-flex__item u-padding-tiny u-margin-left" href="#">
                 <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--inst" aria-hidden="true">
                 </span><span class="u-hidden-visually">Instagram</span>
         </a>
