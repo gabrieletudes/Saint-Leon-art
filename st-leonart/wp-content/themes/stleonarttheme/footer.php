659414654ea7f2dd95896d8faa77ec33ef662d1c
Gabriel M <gabrieletudes@gmail.com>
2018-01-20 15:53:40 +0100

    Added theme asset

--- st-leonart/wp-content/themes/stleonarttheme/footer.php
+++ st-leonart/wp-content/themes/stleonarttheme/footer.php
@@ -66,5 +66,7 @@
 </div><!--END Hidder-->
 <script type="text/javascript" src="./assets/js/jquery-2.2.1.min.js"></script>
 <script type="text/javascript" src="./assets/js/script.js"></script>
+<script type="text/javascript" src="<?php theme_asset('/js/jquery-2.2.1.min.js');?>"></script>
+<script type="text/javascript" src="<?php theme_asset('/js/script.js');?>"></script>
 </body>
 </html>
