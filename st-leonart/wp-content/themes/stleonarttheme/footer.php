c3526bd3263270ac9400131cb521e356b28ae898
Gabriel M <gabrieletudes@gmail.com>
2018-01-20 15:56:56 +0100

    Fixed __() in case of translation in the futur.

--- st-leonart/wp-content/themes/stleonarttheme/footer.php
+++ st-leonart/wp-content/themes/stleonarttheme/footer.php
@@ -1,12 +1,12 @@
 <div class="">
   <footer class="c-footer o-content-wrapper u-padding-vertical-large c-bg-color--purple">
     <div class="o-flex o-flex--wrap">
-        <div class="o-layout__item  u-1/1@mobile u-3/12@desktop o-flex o-flex--wrap u-padding-right-small o-flex__item">
+        <div class="o-layout__item  u-1/1@mobile u-3/12@desktop o-flex o-flex--wrap u-padding-right-small o-flex__item u-margin-bottom">
           <h4 class="u-hidden-visually c-h--white">Footer</h4>
           <a href="#" class="o-flex o-flex--lock-left u-margin-bottom"><img class="o-flex--lock-left" src="assets/img/logo.svg" alt="Logo" width="62" height="49"></a>
           <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est eopksio laborum. Sed ut perspiciatis unde omnis istpoe natus error sit voluptatem.</p>
           <div class="u-1/1">
-            <h4 class="c-h--white">Suivez-nous sur</h4>
+            <h4 class="c-h--white"><?= __('Suivez-nous sur','stla')?></h4>
             <div class="o-flex o-flex--wrap c-social-nav o-flex--centered">
               <a class="c-social-nav__item u-1/4  o-flex__item" href="#">
                 <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--fab" aria-hidden="true">
@@ -28,7 +28,7 @@
         </div>
       </div>
         <div class="o-layout__item  u-1/1@mobile u-4/12@tablet u-2/12@desktop u-padding-right-small o-flex__item">
-          <h4 class="u-margin-bottom-small c-h--white">Menu</h4>
+          <h4 class="u-margin-bottom-small c-h--white"><?=__('Menu','stla')?></h4>
           <ul class="o-list-bare">
             <li><a href="#0">Accueil</a></li>
             <li><a href="#4">Programme</a></li>
@@ -40,7 +40,7 @@
           </ul>
         </div>
         <div class="o-layout__item  u-1/1@mobile u-6/12@tablet u-3/12@desktop u-padding-right-small o-flex__item">
-          <h4 class="u-margin-bottom-small c-h--white">Contactez-nous</h4>
+          <h4 class="u-margin-bottom-small c-h--white"><?=__('Contactez-nous','stla');?></h4>
           <ul class="o-list-bare">
             <li>36, Place Vivegnis 4000 Liège</li>
             <li><a href="tel:+32493121421">+32 493 121 421</a></li>
@@ -48,24 +48,22 @@
           </ul>
         </div>
         <div class="c-newsletter o-layout__item  u-1/1@mobile u-3/4@tablet u-4/12@desktop c-bg-color--shadow-white u-padding u-padding-bottom-large o-flex o-flex--wrap">
-          <h4 class="c-h--purple">NEWSLETTER</h4>
+          <h4 class="c-h--purple"><?= __('NEWSLETTER','stla');?></h4>
           <p class="c-h--purple u-1/1">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est eopksio laborum. </p>
         <form action="" class="c-newsletter-form o-flex o-flex--wrap">
           <input class="c-newsletter-form__input u-padding-small" type="text" name="" placeholder="marco.polo@email.com" value="">
-          <button class="o-flex o-flex--centered-v c-btn c-btn--submit c-btn--small c-btn--primary" type="submit" name="">Souscrir</button>
+          <button class="o-flex o-flex--centered-v c-btn c-btn--submit c-btn--small c-btn--primary" type="submit" name=""><?=__('Souscrir','stla');?></button>
         </form>
         </div>
       </div>
     </footer>
     <p class="o-content-wrapper u-padding-vertical-small u-margin-bottom-none c-bg-color--purple-light-10 o-flex o-flex--wrap o-flex--centered">
-      <small class="o-flex__item u-margin-right u-margin-right@mobile-none">Copyright © 2017 Saint Leon’art. Tous droits réservés.</small>
-      <small class="o-flex__item">Crafted with <span class="c-footer__icon--heart">love</span> by <a class="c-footer__maker-link" href="http://martinz.be">MARTINZ</a></small>
+      <small class="o-flex__item u-margin-right u-margin-right@mobile-none"><?= __('Copyright © 2017 Saint Leon’art. Tous droits réservés.','stla');?></small>
+      <small class="o-flex__item"><?=__('Crafted with ','stla');?><span class="c-footer__icon--heart"><?=__('love','stla');?></span><?= __(' by ','stla');?><a class="c-footer__maker-link" href="http://martinz.be">MARTINZ</a></small>
     </p>
   </div>
 </div><!--END Site Pusher-->
 </div><!--END Hidder-->
-<script type="text/javascript" src="./assets/js/jquery-2.2.1.min.js"></script>
-<script type="text/javascript" src="./assets/js/script.js"></script>
 <script type="text/javascript" src="<?php theme_asset('/js/jquery-2.2.1.min.js');?>"></script>
 <script type="text/javascript" src="<?php theme_asset('/js/script.js');?>"></script>
 </body>
