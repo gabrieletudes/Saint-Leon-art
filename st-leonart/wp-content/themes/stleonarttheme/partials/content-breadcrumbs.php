cf04de3d1840d1cc6120230bf6375267826aabf0
Gabriel M <gabrieletudes@gmail.com>
2018-01-22 18:18:43 +0100

    Added breadcrumbs partial

---
+++ st-leonart/wp-content/themes/stleonarttheme/partials/content-breadcrumbs.php
@@ -0,0 +1,6 @@
+<div class="u-1/1 c-h--white u-margin-top">
+    <div class="breadcrumbs-section" typeof="BreadcrumbList" vocab="http://schema.org/">
+        <?php if(function_exists('bcn_display'))
+        { bcn_display(); }?>
+    </div>
+</div>
