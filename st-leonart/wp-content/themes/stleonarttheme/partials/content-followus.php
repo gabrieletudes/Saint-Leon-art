1c78f70c572829bead496644348cdad8a7eb6031
Gabriel M <gabrieletudes@gmail.com>
2018-01-22 19:54:40 +0100

    Fixed span tags

--- st-leonart/wp-content/themes/stleonarttheme/partials/content-followus.php
+++ st-leonart/wp-content/themes/stleonarttheme/partials/content-followus.php
@@ -4,22 +4,24 @@
         <?php $social_fb = get_field('contact_fb',stla_get_page_id('contact.php'));?>
         <?php if($social_fb):?>
         <a class="c-social-nav__item o-flex__item u-padding-tiny" href="<?= $social_fb?>" title="<?= __('Suivez nous sur Facebook', 'stla') ?>">
-                <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--fab" aria-hidden="true">
-                </span><span class="u-hidden-visually">Facebook</span>
+                <span class="c-social-icon c-social-icon--fab" aria-hidden="true">
+                    <span class="u-hidden-visually">Facebook</span>
+                </span>
         </a>
         <?php endif;?>
         <?php $social_tw = get_field('contact_tw',stla_get_page_id('contact.php'));?>
         <?php if($social_tw):?>
         <a class="c-social-nav__item o-flex__item u-padding-tiny u-margin-left" href="<?= $social_tw?>" title="<?= __('Suivez nous sur Twitter', 'stla') ?>">
-                <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--twtr" aria-hidden="true">
-                </span><span class="u-hidden-visually">Twitter</span>
+                    <span class="c-social-icon c-social-icon--twtr" aria-hidden="true">
+                        <span class="u-hidden-visually">Twitter</span>
+                    </span>
         </a>
             <?php endif?>
         <?php $social_inst = get_field('contact_insta',stla_get_page_id('contact.php'));?>
         <?php if($social_inst):?>
         <a class="c-social-nav__item o-flex__item u-padding-tiny u-margin-left" href="<?= $social_inst?>" title="<?= __('Suivez nous sur Instagram', 'stla') ?>">
-                <span class="icon-fallback-text"><span class="c-social-icon c-social-icon--inst" aria-hidden="true">
-                </span><span class="u-hidden-visually">Instagram</span>
+                <span class="c-social-icon c-social-icon--inst" aria-hidden="true">
+                    <span class="u-hidden-visually">Instagram</span></span>
         </a>
         <?php endif;?>
     </div>
