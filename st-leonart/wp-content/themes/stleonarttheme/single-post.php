b0b282393ea3a37f6253445b6dbadf99ff3f05b7
Gabriel M <gabrieletudes@gmail.com>
2018-01-22 19:55:23 +0100

    Replaced p tag by time tag

--- st-leonart/wp-content/themes/stleonarttheme/single-post.php
+++ st-leonart/wp-content/themes/stleonarttheme/single-post.php
@@ -10,7 +10,7 @@ $type = get_field('article_type');
 <div class="o-content-wrapper o-content-wrapper--max o-flex o-flex--wrap u-padding-bottom u-padding-top-large">
 <h2 class="u-10/12@tablet u-8/12@desktop u-6/12@wide"><?= $post->post_title;?></h2>
 <div class="o-flex o-flex--lock-top-left u-1/1 u-margin-bottom-small">
-  <p class="c-article__date u-margin-bottom-tiny"><?= $date->format('j M Y');?></p>
+  <time datetime="<?= $date->format('Y-m-j');?>" class="c-article__date u-margin-bottom-tiny"><?= $date->format('j M Y');?></time>
   <p class="c-article__label u-margin-bottom-tiny"><?= $type->name;?></p>
 </div>
 <div class="u-1/1@mobile u-10/12@tablet u-8/12@desktop u-7/12@wide c-article">
