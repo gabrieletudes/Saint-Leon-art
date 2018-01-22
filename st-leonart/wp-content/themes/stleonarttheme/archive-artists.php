747313979c34192d800f8f7dcccc4e696f59e4c7
Gabriel M <gabrieletudes@gmail.com>
2018-01-22 18:00:58 +0100

    Added artist picture

--- st-leonart/wp-content/themes/stleonarttheme/archive-artists.php
+++ st-leonart/wp-content/themes/stleonarttheme/archive-artists.php
@@ -11,9 +11,7 @@ if($_GET['profession'] && !empty($_GET['profession'] && in_array($_GET['professi
     $theskill = $theskills;
 }
 
-$mycurrentpage = (get_query_var('paged', 1));
 $args = [
-    'posts_per_page' => 6,
     'post_type' => 'artists',
     'tax_query' => [
             [
@@ -25,7 +23,6 @@ $args = [
 ];
 // query
 $the_query = new WP_Query($args);
-//$total_pages = $the_query->max_num_pages;
 ?>
 <?php if ($the_query->have_posts()): ?>
 <div class="o-content-wrapper c-bg-color--purple-light-20 o-flex o-flex--wrap u-padding-vertical-large c-bg-color--purple-light-40">
@@ -43,7 +40,7 @@ $the_query = new WP_Query($args);
                     <?php $skills = get_terms('skills');?>
                     <?php foreach($skills as $skill): ?>
                     <li tabindex="-1" class="o-flex u-1/1">
-                        <a class="u-1/1 u-padding-small" id="titina" href="<?= the_permalink().'?profession='.$skill->name;?>"><?= $skill->name ?></a>
+                        <a class="u-1/1 u-padding-small" href="<?= the_permalink().'?profession='.$skill->name;?>"><?= $skill->name ?></a>
                     </li>
                     <?php endforeach;?>
                 </ul>
@@ -55,9 +52,18 @@ $the_query = new WP_Query($args);
     <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
                 <a href="<?= the_permalink()?>"
                    class="c-artistcard u-padding-right-small u-margin-bottom-small u-1/2@mobile u-1/4@tablet u-1/4@desktop u-2/12@wide">
-                    <img class="c-artistcard__image u-margin-bottom-small o-flex__item" src="http://fillmurray.com/320/320" alt="there is not image yet">
+                    <?php $artistimage = get_field('artist_picture');
+                    $thumb = $artistimage['sizes']['artist-medium'];
+                    $alt = $artistimage['alt'];
+                    ?>
+                    <?php if($artistimage):?>
+                        <img class="c-artistcard__image u-margin-bottom-small o-flex__item" src="<?=$thumb?>" alt="<?=$alt?>" width="320" height="320">
+                    <?php else:?>
+                    <img class="c-artistcard__image u-margin-bottom-small o-flex__item" src="http://fillmurray.com/320/320" alt="t<?= __('Image temporaire pour l’artiste','stla')?>" width="320" height="320">
+                    <?php endif;?>
                     <span class="c-artistcard__name o-flex"><?= $post->post_title;?></span>
                     <?php if( $skills ): ?>
+                        <span class="u-hidden-visually"><?=__('Profession de l’artiste ','stla');?></span>
                     <span class="c-artistcard__ability o-flex">
                        <?= stla_the_skills($post->ID,', ');?>
                     </span>
